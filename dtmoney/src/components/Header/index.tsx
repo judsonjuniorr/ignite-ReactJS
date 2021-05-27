a4c0c20fe106ae64ef52963ad269e3feceb30012
Judson Junior <judsonjuniorr@gmail.com>
2021-05-27 19:01:33 -0300

    Configurando modal de criação

--- dtmoney/src/components/Header/index.tsx
+++ dtmoney/src/components/Header/index.tsx
@@ -2,12 +2,18 @@ import logo from "../../assets/logo.svg";
 
 import * as S from "./styles";
 
-export function Header() {
+interface IHeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: IHeaderProps) {
   return (
     <S.Container>
       <S.Content>
         <img src={logo} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </S.Content>
     </S.Container>
   );
