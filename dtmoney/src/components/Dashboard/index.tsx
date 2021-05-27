41c5855967e7c4b8030bfbb83558dc433575f9a7
Judson Junior <judsonjuniorr@gmail.com>
2021-05-27 18:04:10 -0300

    Componente: TransactionsTable

--- dtmoney/src/components/Dashboard/index.tsx
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import * as S from "./styles";
 
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
 
 export function Dashboard() {
   return (
     <S.Container>
       <Summary />
+      <TransactionsTable />
     </S.Container>
   );
 }
