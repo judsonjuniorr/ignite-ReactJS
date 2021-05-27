5e7658ed877aeca51556af876b5d583f83d6e992
Judson Junior <judsonjuniorr@gmail.com>
2021-05-27 16:49:57 -0300

    Componente: Summary

---
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+import { Summary } from "../Summary";
+
+export function Dashboard() {
+  return (
+    <S.Container>
+      <Summary />
+    </S.Container>
+  );
+}
